65a30d8498a1fef2b8b532c1b85841e8b9825391
Aniket Baranwal <aniket.baranwal@incture.com>
2025-10-09 10:15:53 +0530

    Added PO queries

---
+++ client/src/api/queries/purchaseOrder.js
@@ -0,0 +1,31 @@
+import axiosClient from "../axiosClient";
+
+const base = "purchaseOrder"; 
+
+export const createPurchaseOrder = (data) => {
+  return axiosClient.post(`/${base}`, data);
+};
+
+export const getAllPurchaseOrders = () => {
+  return axiosClient.get(`/${base}`);
+};
+
+export const getPurchaseOrderById = (id) => {
+  return axiosClient.get(`/${base}/${id}`);
+};
+
+export const submitPurchaseOrder = (id) => {
+  return axiosClient.patch(`/${base}/${id}/submit`);
+};
+
+export const getApproverOrders = () => {
+  return axiosClient.get(`/${base}/approver`);
+};
+
+export const approvePurchaseOrder = (id, data) => {
+  return axiosClient.patch(`/${base}/approver/${id}/approve`, data);
+};
+
+export const rejectPurchaseOrder = (id, data) => {
+  return axiosClient.patch(`/${base}/approver/${id}/reject`, data);
+};
